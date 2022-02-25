b915e98827cce53f71b90f6b42616f81971864c5
hyeon <selwha1@naver.com>
2022-02-25 18:53:33 +0900

    데이터 model 수정

--- src/musicsite/music/models.py
+++ src/musicsite/music/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class Music(models.Model):
     title = models.CharField(max_length=100) #제목
     singer = models.CharField(max_length=30) #가수
-    album = models.CharField(max_length=30) #앨범
-    genre = models.CharField(max_length=30) #장르
-    lyrics = models.CharField(max_length=1000) #가사
-    lyricists = models.CharField(max_length=100) #작사,작곡가들
-    release_date = models.DateField() #발매일
+    album = models.CharField(max_length=30, null = True) #앨범
+    genre = models.CharField(max_length=30, null = True) #장르
+    lyrics = models.CharField(max_length=1000, null = True) #가사
+    lyricists = models.CharField(max_length=100, null = True) #작사,작곡가들
+    release_date = models.DateField(null = True) #발매일
