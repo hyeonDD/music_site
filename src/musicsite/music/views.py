24d52b2bd80fd8ee0fc85d54a9a85f3c6a49b16d
hyeon <selwha1@naver.com>
2022-02-26 18:00:03 +0900

    플레이리스트를 가져오도록 변경

--- src/musicsite/music/views.py
+++ src/musicsite/music/views.py
@@ -1,5 +1,11 @@
 # from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Music
 # Create your views here.
 def index(request):
-    return HttpResponse("ㅎㅇㅎㅇ")
+    """
+    음악 목록 출력
+    """
+    play_list = Music.objects.order_by('-release_date')
+    context = {'play_list': play_list}
+    return render(request, 'play_list.html', context)
