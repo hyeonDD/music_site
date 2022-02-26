6538be8ef1ba69c8514ff902588064d5a5833cbe
hyeon <selwha1@naver.com>
2022-02-26 18:29:19 +0900

    URL 네임스페이스 적용

--- src/musicsite/music/urls.py
+++ src/musicsite/music/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'music'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:music_id>/', views.detail, name='detail'),
