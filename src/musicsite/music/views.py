3f45003bb4db7ce251ce66262f0c74aa134b50ef
hyeon <selwha1@naver.com>
2022-02-26 18:27:10 +0900

    노래 상세페이지 생성

--- src/musicsite/music/views.py
+++ src/musicsite/music/views.py
@@ -1,5 +1,5 @@
 # from django.shortcuts import render
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Music
 # Create your views here.
 def index(request):
@@ -9,3 +9,12 @@ def index(request):
     play_list = Music.objects.order_by('-release_date')
     context = {'play_list': play_list}
     return render(request, 'play_list.html', context)
+
+def detail(request, music_id):
+    """
+    음악 내용 출력
+    """
+
+    music = get_object_or_404(Music, pk=music_id)
+    context = {'music': music}
+    return render(request, 'music_detail.html', context)
