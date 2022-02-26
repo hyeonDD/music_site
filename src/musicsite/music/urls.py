d9b956f740b42ede0d4b32184737d0771d6fb705
hyeon <selwha1@naver.com>
2022-02-26 18:26:34 +0900

    URL 별칭 사용을 지원

--- src/musicsite/music/urls.py
+++ src/musicsite/music/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('<int:music_id>/', views.detail, name='detail'),
 ]
