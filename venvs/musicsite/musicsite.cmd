2048b2e6b2e258d379964b7fc9ae6dccedc7f44f
hyeon <selwha1@naver.com>
2022-02-28 16:45:48 +0900

    python 가상환경

--- venvs/musicsite/musicsite.cmd
+++ venvs/musicsite/musicsite.cmd
@@ -1,4 +1,4 @@
 @echo off
 d:
-cd d:/code/vscode/github/music_django/src/musicsite
+cd D:/code/vscode/github/music_django/src/musicsite
 d:/code/vscode/github/music_django/venvs/musicsite/scripts/activate
