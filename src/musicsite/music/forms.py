c84b9af3ac93b313d63f0ce66d6349ad9a0d65ac
hyeon <selwha1@naver.com>
2022-03-06 15:13:26 +0900

    음악 등록시 등록할 폼항목 변경

--- src/musicsite/music/forms.py
+++ src/musicsite/music/forms.py
@@ -4,9 +4,21 @@ from music.models import Music
 class MusicForm(forms.ModelForm):
     class Meta:
         model = Music
-        fields = ('title', 'singer', 'album', 'genre', 'lyrics'\
-                 ,'lyricists', 'release_date', 'URL')
-        widgets = {
-            'title' : forms.TextInput(attrs={'class':'form-control'}),
-            'singer' : forms.TextInput(attrs={'class':'form-control'}),
+        # fields = ('title', 'singer', 'album', 'genre', 'lyrics'\
+        #          ,'lyricists', 'release_date', 'URL')        
+        # labels = {
+        #     'title': '제목',
+        #     'singer': '가수',
+        #     'album': '앨범',
+        #     'genre': '장르',
+        #     'lyrics': '가사',
+        #     'lyricists': '작사',
+        #     'release_date': '발매일',
+        #     'URL': 'Youtube URL',
+        # }
+        fields = ('title', 'singer', 'URL')        
+        labels = {
+            'title': '제목',
+            'singer': '가수',            
+            'URL': 'Youtube URL',
         }
