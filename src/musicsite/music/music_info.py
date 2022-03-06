655d9663836f2873831642509cbfd9b9a2201e22
hyeon <selwha1@naver.com>
2022-03-06 15:13:08 +0900

    멜론에서 곡 정보 가져오는 스크립트 작성

---
+++ src/musicsite/music/music_info.py
@@ -0,0 +1,59 @@
+import requests
+from bs4 import BeautifulSoup
+import functools
+import operator
+
+class MusicInfo:
+    def __init__(self, URL='') -> None:
+        headers = {
+            'User-Agent':'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'}        
+        # URL = f'https://www.melon.com/search/total/index.htm?q=사랑비김태우'        
+        # URL = f'https://www.melon.com/search/total/index.htm?q=허각 바다'        
+        html = requests.get(URL, headers=headers)
+        self.soup = BeautifulSoup(html.content, 'html.parser')
+
+    def parsing(self, selector):                
+        datas = self.soup.select_one(f'{selector}')
+        return datas
+
+def musicInfoRun(search):
+    get_detail_URL = MusicInfo(f'https://www.melon.com/search/song/index.htm?q={search}') #클래스 이름으로 검색
+    tmp = str(get_detail_URL.parsing('.btn_icon_detail')) # str형으로 변환하기위해 잠시저장
+    benchmark = tmp.rindex('goSongDetail') # goSongDetail 지점 찾기
+    benchmark2 = tmp.rindex("');")
+    tmp = tmp[benchmark+14:benchmark2] # 지점을 기준으로 짜름 (SongID가 짤려서나옴)    
+
+    get_detail_URL = MusicInfo('https://www.melon.com/song/detail.htm?songId='+tmp)    
+    music_title = str(get_detail_URL.parsing('.song_name').text.split()[1:]) # 노래 이름
+    music_singer = get_detail_URL.parsing('.artist').text.strip() # 노래 가수    
+    _, music_album, _ , release_date, _, music_genre, *_ = get_detail_URL.parsing('.meta').text.strip().split('\n') # 앨범, 발매일, 장르
+    try :
+        music_lyrics = get_detail_URL.parsing('.lyric').text.strip() # 노래 가사
+    except AttributeError:
+        print("노래가사 준비중")
+        music_lyrics = '- 노래가사 준비중 -'
+
+    music_lyricists = get_detail_URL.soup.select('.entry > .ellipsis') # 작사/작곡만 여러개 조회를 위해 select 사용
+    li_music_lyricists = [i.text.strip() for i in music_lyricists]
+    music_lyricists_detail = get_detail_URL.soup.select('.meta span.type') # 작사/작곡만 여러개 조회를 위해 select 사용
+    li_music_lyricists_detail = [i.text.strip() for i in music_lyricists_detail]
+    try :
+        music_lyricists_detail = str(functools.reduce(operator.add,list(zip(li_music_lyricists_detail,li_music_lyricists)))) # 작사/작곡 묶기
+    except TypeError:
+        print("작사/작곡 준비중")
+        music_lyricists_detail= '- 작사/작곡 준비중 -'
+
+    return music_title, music_singer, music_album, release_date.replace('.','-'), music_genre, music_lyrics, music_lyricists_detail
+    
+
+
+
+if __name__ == '__main__':  
+    # a, *_ = musicInfoRun('러브블러썸 케이윌')
+    # a, *_ = musicInfoRun('사랑비 김태우')
+    
+    _,_,_,a,*_ = musicInfoRun('러브119 케이윌')
+    print(a)
+    print(type(a))
+
+    # musicInfoRun('러브119 케이윌')
