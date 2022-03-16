e38b538db7bd72fe84e5d56dcccaff2b6effda06
hyeon <selwha1@naver.com>
2022-03-16 09:02:21 +0900

    썸네일 이미지파일 다운로드 추가

--- src/musicsite/music/music_info.py
+++ src/musicsite/music/music_info.py
@@ -3,18 +3,32 @@ from bs4 import BeautifulSoup
 import functools
 import operator
 
+STATIC_THUMBNAIL_PATH = 'src/musicsite/music/static/thumbnail/'
+
 class MusicInfo:
     def __init__(self, URL='') -> None:
-        headers = {
+        self.headers = {
             'User-Agent':'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'}        
         # URL = f'https://www.melon.com/search/total/index.htm?q=사랑비김태우'        
         # URL = f'https://www.melon.com/search/total/index.htm?q=허각 바다'        
-        html = requests.get(URL, headers=headers)
+        html = requests.get(URL, headers=self.headers)
         self.soup = BeautifulSoup(html.content, 'html.parser')
 
     def parsing(self, selector):                
         datas = self.soup.select_one(f'{selector}')
         return datas
+    
+    def download_thumbnail(self, url, filename):
+        thumbnail_url = str(url.parsing('.thumb > .image_typeAll'))
+
+        benchmark = thumbnail_url.rindex('https://cdnimg.melon.co.kr/') # 썸네일 url 지점 찾기
+        benchmark2 = thumbnail_url.rindex("optimize")
+        thumbnail_url = thumbnail_url[benchmark:benchmark2+8] # 지점을 기준으로 짜르기        
+   
+        with open(STATIC_THUMBNAIL_PATH+filename+'.jpg', 'wb') as file:
+            res = requests.get(thumbnail_url, headers=self.headers)
+            img_data = res.content
+            file.write(img_data)
 
 def musicInfoRun(search):
     get_detail_URL = MusicInfo(f'https://www.melon.com/search/song/index.htm?q={search}') #클래스 이름으로 검색
@@ -44,18 +58,19 @@ def musicInfoRun(search):
     except TypeError:
         print("작사/작곡 준비중")
         music_lyricists_detail= '- 작사/작곡 준비중 -'
+    
+    get_detail_URL.download_thumbnail(get_detail_URL, music_title) # 썸네일 이미지 다운로드
 
     return music_title, music_singer, music_album, release_date.replace('.','-'), music_genre, music_lyrics, music_lyricists_detail
     
 
-
-
 if __name__ == '__main__':  
-    # a, *_ = musicInfoRun('러브블러썸 케이윌')
+    """ # a, *_ = musicInfoRun('러브블러썸 케이윌')
     a, *_ = musicInfoRun('사랑비 김태우')
     
     # _,_,_,a,*_ = musicInfoRun('러브119 케이윌')
     print(a)
     print(type(a))
 
-    # musicInfoRun('러브119 케이윌')
+    # musicInfoRun('러브119 케이윌') """
+    musicInfoRun('러브블러썸 케이윌')
