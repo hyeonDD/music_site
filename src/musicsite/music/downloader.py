38ac0d2dd144d46aca313c75f58c14054f7678e0
hyeon <selwha1@naver.com>
2022-03-17 18:11:23 +0900

    노래url 자동으로 찾아오기 추가

--- src/musicsite/music/downloader.py
+++ src/musicsite/music/downloader.py
@@ -1,10 +1,23 @@
 import os
 import youtube_dl
+from youtube_search import YoutubeSearch
+import json
+import os
 
-VIDEO_DOWNLOAD_PATH = f"{os.path.dirname(os.path.realpath(__file__))}\\static\\audio"  # 다운로드 경로
-# print(VIDEO_DOWNLOAD_PATH)
+VIDEO_DOWNLOAD_PATH = f"{os.path.dirname(os.path.realpath(__file__))}/static/audio"
+print(VIDEO_DOWNLOAD_PATH)
+
+def search_url(search):
+    results = YoutubeSearch(search, max_results=10).to_json()
+    result = json.loads(results) # 결과json을 dict 형으로 바꿈
+    result = result["videos"][0]["url_suffix"] # 결과로 /watch?v= ??? 이나옴
+    return result
 
 def download_video_and_subtitle(video_url, file_name):
+    if os.path.isfile(f"{VIDEO_DOWNLOAD_PATH}/{file_name}.mp3"):
+        print("파일이 이미 존재합니다")
+        return 0
+
     download_path = os.path.join(VIDEO_DOWNLOAD_PATH, file_name+'.%(ext)s')
 
     # for video_url in youtube_video:
@@ -27,9 +40,14 @@ def download_video_and_subtitle(video_url, file_name):
     except Exception as e:
         print('error', e)
 
+def downloader_run(search):
+    url = search_url(search)
+    download_video_and_subtitle("https://www.youtube.com"+url,url[9:])
 
 if __name__ == '__main__':
 
-    youtube_url_list = "https://www.youtube.com/watch?v=fFrnO4HqtI8" # 유투브에서 다운로드 하려는 영상의 주소 리스트(Sample Video URL)
-    download_video_and_subtitle(youtube_url_list,'when2')
-    print('Complete download!')
+    # url = search_url('사랑비 김태우')
+    # print(url)
+    # download_video_and_subtitle("https://www.youtube.com"+url,url[9:])
+    # print('Complete download!')
+    downloader_run('사랑비 김태우')
